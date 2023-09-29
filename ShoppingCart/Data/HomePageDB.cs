d6ce627b696e2e0d663aa12161fbb00361cef4c2
Eswar <eswarsatishk@gmail.com>
2023-09-29 12:24:13 +0530

    Added Id in HomePageModel and created Homepagedb file

---
+++ ShoppingCart/Data/HomePageDB.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingCart.Data
+{
+    public class HomePageDB : DbContext
+    {
+        public HomePageDB(DbContextOptions<HomePageDB> options) : base(options)
+        {
+            
+        }
+
+    }
+}
--- ShoppingCart/Models/HomePageModel.cs
+++ ShoppingCart/Models/HomePageModel.cs
@@ -2,9 +2,10 @@
 {
     public class HomePageModel
     {
+        public int Id { get; set; }
         public string ImageTitle { get; set; }
         public string Name { get; set; }
         public float Price { get; set; }
-        public float description { get; set; }
+        public string description { get; set; }
     }
 }
