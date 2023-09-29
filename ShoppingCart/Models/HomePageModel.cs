0ba7e75d8bf1d3d227ac1f0f9349e649a37d1fc0
Eswar <eswarsatishk@gmail.com>
2023-09-29 10:33:34 +0530

    Added Home page Model and deleted privacy page saved

--- ShoppingCart/Models/HomePageModel.cs
+++ ShoppingCart/Models/HomePageModel.cs
@@ -2,5 +2,9 @@
 {
     public class HomePageModel
     {
+        public string ImageTitle { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public float description { get; set; }
     }
 }
